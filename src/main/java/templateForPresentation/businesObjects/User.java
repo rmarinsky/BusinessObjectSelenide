c6d4648c7a4b0bea3e633729827a4896f747a2da
rmarinsky <HB_19wRM>
2017-03-07 13:49:39 +0200

    rename all constants By

--- src/main/java/templateForPresentation/businesObjects/User.java
+++ src/main/java/templateForPresentation/businesObjects/User.java
@@ -10,65 +10,65 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class User {
 
-    public final By FIELD_LOGIN = by("name*", "username"),
-            FIELD_PASSWORD = by("name*", "password"),
-            FIELD_FIRST_NAME = by("name*", "firstName"),
-            FIELD_LAST_NAME = by("name*", "lastName"),
-            FIELD_EMAIL = by("name*", "email"),
-            FIELD_CITY = byCssSelector("input#city"),
-            FIELD_MIDDLE_NAME = by("name*", "middleName"),
-            CUSTOM_COUNTRY = byCssSelector("#country_id_chosen"),
-            CUSTOM_REGION = byCssSelector("#state_id_chosen"),
-            CUSTOM_CITY = byCssSelector("#city_id_chosen"),
-            FIELD_ADDRESS = by("name*", "[address]"), //because of element have name = "address[address]"
-            CUSTOM_PHONE_CODES = byCssSelector("a.styled-phone-dropdown-button"),
-            FIELD_PHONE_NUMBER = byCssSelector(".styled-phone-edit > input");
+    public final By loginField = by("name*", "username"),
+            paswField = by("name*", "password"),
+            firstNameField = by("name*", "firstName"),
+            lastNameField = by("name*", "lastName"),
+            emailField = by("name*", "email"),
+            cityField = byCssSelector("input#city"),
+            middleNameField = by("name*", "middleName"),
+            countryCustom = byCssSelector("#country_id_chosen"),
+            regionCustom = byCssSelector("#state_id_chosen"),
+            cityCustom = byCssSelector("#city_id_chosen"),
+            addressField = by("name*", "[address]"), //because of element have name = "address[address]"
+            phoneCodeCustom = byCssSelector("a.styled-phone-dropdown-button"),
+            phoneNumberCutom = byCssSelector(".styled-phone-edit > input");
 
     public User fillLogin(String loginValue){
-        $(FIELD_LOGIN).sendKeys(loginValue);
+        $(loginField).sendKeys(loginValue);
         return this;
     }
 
     public User fillPasw (String passwordValue){
-        $(FIELD_PASSWORD).sendKeys(passwordValue);
+        $(paswField).sendKeys(passwordValue);
         return this;
     }
 
     public User fillFirstName (String firstNameValue){
-        $(FIELD_FIRST_NAME).setValue(firstNameValue);
+        $(firstNameField).setValue(firstNameValue);
         return this;
     }
 
     public User fillLastName (String lastNameValue){
-        $(FIELD_LAST_NAME).setValue(lastNameValue);
+        $(lastNameField).setValue(lastNameValue);
         return this;
     }
 
     public User fillMiddleName (String middleNameValue){
-        $(FIELD_MIDDLE_NAME).setValue(middleNameValue);
+        $(middleNameField).setValue(middleNameValue);
         return this;
     }
 
     public User fillEmail (String emailValue){
-        $(FIELD_EMAIL).setValue(emailValue);
+        $(emailField).setValue(emailValue);
         return this;
     }
 
     public User fillCity (String cityValue){
-        $(FIELD_CITY).setValue(cityValue);
+        $(cityField).setValue(cityValue);
         return this;
     }
 
     public User selectCountry(String countryValue){
-        return this.chooseCustom(CUSTOM_COUNTRY, countryValue);
+        return this.chooseCustom(countryCustom, countryValue);
     }
 
     public User selectRegion(String regionValue){
-        return this.chooseCustom(CUSTOM_REGION, regionValue);
+        return this.chooseCustom(regionCustom, regionValue);
     }
 
     public User selectCity(String cityValue){
-        return this.chooseCustom(CUSTOM_CITY, cityValue);
+        return this.chooseCustom(cityCustom, cityValue);
     }
 
     private User chooseCustom(final By customElement, final String value){
@@ -78,14 +78,14 @@ public class User {
     }
 
     public User fillAddress(String addressValue){
-        $(FIELD_ADDRESS).setValue(addressValue);
+        $(addressField).setValue(addressValue);
         return this;
     }
 
     public User fillPhoneByCountry(final String country, final String phone){
-        $(this.CUSTOM_PHONE_CODES).click();
+        $(this.phoneCodeCustom).click();
         $(String.format("b[data-hint='%s']", country)).scrollTo().click();
-        $(this.FIELD_PHONE_NUMBER).setValue(phone);
+        $(this.phoneNumberCutom).setValue(phone);
         return this;
     }
 
